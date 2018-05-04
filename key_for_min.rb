b70f15242271e4e8e97b4d4c1b18cfa24c8d15b1
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-04 18:44:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value(name_hash)
   min_value = nil
   name_hash.each do |key, value|
     if min_value == nil || value < min_value
-      min_key = key 
+      min_key = key
       min_value = value
-    end 
+    end
   end
   min_key
 end
